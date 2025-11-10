14c190c641ae8830b6f004542373f18cf06c0bf9
manthan <manthan.jsharma@gmail.com>
2025-11-10 17:38:12 +0530

    fix: middleware issue

--- server/server.js
+++ server/server.js
@@ -10,9 +10,22 @@ dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
+const whitelist = [
+  "http://localhost:3000",
+  "hhttps://visa-application-brown.vercel.app/",
+];
 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(join(__dirname, "../client/build")));
 
